4a16c09a84fb38efe6ec832b2b6cada0f807a89e
zzh1991 <zzh19910530@163.com>
2019-06-07 23:59:24 +0800

    add new layout

--- routes.js
+++ routes.js
@@ -1,9 +1,10 @@
 import React, { Suspense, lazy } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import SideBar from './components/sidebar.js';
+// import SideBar from './components/sidebar.js';
 import NotFound from './containers/notFound';
+import MoviePageContainer from './containers/MoviePageContainer';
 
 const SideBarContainer = lazy(() => import('./containers/SidebarContainer'));
 const TopMoviesContainer = lazy(() => import('./containers/TopMoviesContainer'));
@@ -14,16 +15,17 @@ const AllMoviesContainer = lazy(() => import('./containers/AllMoviesContainer'))
 const Routes = () => (
   <Provider store={store}>
     <Router>
-      <SideBar>
+      <MoviePageContainer>
         <Suspense fallback={<div>Loading...</div>}>
-          <Route exact path="/" component={SideBarContainer} />
+          <Route exact path="/" render={() => (<Redirect to="/recent" />)} />
+          <Route path="/recent" component={SideBarContainer} />
           <Route path="/top" component={TopMoviesContainer} />
           <Route path="/view" component={ViewedMoviesContainer} />
           <Route path="/star" component={StarMoviesContainer} />
           <Route path="/all" component={AllMoviesContainer} />
           <Route path="/404" component={NotFound} />
         </Suspense>
-      </SideBar>
+      </MoviePageContainer>
     </Router>
   </Provider>
 );
