f23ccefed1ba49697a72b677c6ffe3125ace6c3f
zzh1991 <zzh19910530@163.com>
2019-08-18 09:26:58 +0800

    update front dependencies

--- webpack.dev.js
+++ webpack.dev.js
@@ -103,6 +103,7 @@ module.exports = {
         target: 'http://localhost:8080',
         secure: false,
         prependPath: false,
+        changeOrigin: false,
       },
     },
     historyApiFallback: true,
